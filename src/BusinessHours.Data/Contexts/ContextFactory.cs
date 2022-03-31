5bc9f06ec684ffdbf9a58c798433829033e934e5
Jean Alves <jdalmeida1@stefanini.com>
2022-03-31 10:34:28 -0300

    feat: add context factory to be used for the migrations

---
+++ src/BusinessHours.Data/Contexts/ContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BusinessHours.Data.Contexts
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=acme.com,1433;Initial Catalog=business_hours_api;User ID=sa;Password=pa55w0rd!;";
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
