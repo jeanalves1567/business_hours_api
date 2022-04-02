5fb9c40223b24dd5bce7ac2cf436b11b16791b0a
Jean Alves <jdalmeida1@stefanini.com>
2022-04-02 08:09:38 -0300

    fix: remove connection string from code

--- src/BusinessHours.Data/Contexts/ContextFactory.cs
+++ src/BusinessHours.Data/Contexts/ContextFactory.cs
@@ -7,8 +7,7 @@ namespace BusinessHours.Data.Contexts
     {
         public AppDbContext CreateDbContext(string[] args)
         {
-            // var connectionString = "Server=acme.com,1433;Initial Catalog=BusinessHoursApi;User ID=sa;Password=pa55w0rd!;";
-            var connectionString = "server=acme.com;port=3306;database=business_hours_api;user=root;Password=pa55w0rd!;Persist Security Info=False;Connect Timeout=300";
+            var connectionString = "<databaseConnectionString>";
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
             // optionsBuilder.UseSqlServer(connectionString);
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
